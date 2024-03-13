11989d48fe6813b38ed4a5f303a80722406611db
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-03-13 02:26:48 +0000

    extensions

--- build_mld.py
+++ build_mld.py
@@ -1,5 +1,9 @@
-"""Run the memory_layout module as a script.
+"""
+Copyright © 2024 Mark Crowe <https://github.com/marcocrowe>. All rights reserved.
+Just a simple script to make use of the memory_layout module 
+for Pipreqs to build the requirements.txt file.
 """
 from memory_layout import __main__
 
-__main__.main()
+# Print memory_layout version
+print("Memory Layout version: ", __main__.sys.version)
